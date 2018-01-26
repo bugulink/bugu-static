40220ce1e936b398c1cf32634fd0af28cc475404
helloyou2012 <helloyou2012@yeah.net>
2018-01-26 18:00:24 +0800

    file list

--- src/models/file.js
+++ src/models/file.js
@@ -31,6 +31,19 @@ export default {
         count,
         list
       };
+    },
+    selectAll(state, selected) {
+      const list = state.list.map(f => ({ ...f, selected }));
+      return { ...state, list };
+    },
+    select(state, id) {
+      const list = state.list.map(f => {
+        if (f.id === id) {
+          return { ...f, selected: !f.selected };
+        }
+        return f;
+      });
+      return { ...state, list };
     }
   }
 };
--- src/pages/Files.js
+++ src/pages/Files.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { remain, humanSize } from '../utils';
+import { remain, humanSize, message } from '../utils';
 
 import './Files.less';
 
-function Files({ list, count, dispatch }) {
+function Files({ list, count, offset, dispatch }) {
+  const selected = list.reduce((p, c) => (p && c.selected), true);
+  const selectAll = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'file/selectAll', payload: !selected });
+  };
+  const select = (e, id) => {
+    e.preventDefault();
+    dispatch({ type: 'file/select', payload: id });
+  };
+  const loadMore = () => {
+    dispatch({ type: 'file/getList' }).catch(() => {
+      message.error('Load files failed! Please try again.');
+    });
+  };
   return (
     <div className="files-section">
       <div className="layout-container">
-        <table className="table">
+        <table className="table table-hover">
           <colgroup>
             <col width="5%" />
             <col width="50%" />
@@ -19,7 +33,13 @@ function Files({ list, count, dispatch }) {
           </colgroup>
           <thead>
             <tr>
-              <th className="center"></th>
+              <th className="center">
+                <a href="#" className="checkbox" onClick={selectAll}>
+                  {selected
+                    ? <i className="icon icon-checked" />
+                    : <i className="icon icon-unchecked" />}
+                </a>
+              </th>
               <th>Name</th>
               <th className="center">Size</th>
               <th className="center">Remain</th>
@@ -33,15 +53,19 @@ function Files({ list, count, dispatch }) {
               const now = Date.now() / 1000;
               const time = Math.round(exp - now);
               return (
-                <tr key={file.id}>
+                <tr key={file.id} className={time <= 0 ? 'disabled' : ''}>
                   <td className="center">
-                    <a href="#">
+                    <a href="#" className="checkbox" onClick={(e) => select(e, file.id)}>
                       {file.selected
                         ? <i className="icon icon-checked" />
                         : <i className="icon icon-unchecked" />}
                     </a>
                   </td>
-                  <td>{file.name}</td>
+                  <td>
+                    <div className="ellipsis">
+                      <a href={file.url} title={file.name} download>{file.name}</a>
+                    </div>
+                  </td>
                   <td className="center">{humanSize(file.size, 1)}</td>
                   <td className="center">{remain(time)}</td>
                   <td className="center">{date.toLocaleString()}</td>
@@ -50,16 +74,18 @@ function Files({ list, count, dispatch }) {
             })}
           </tbody>
         </table>
+        {offset < count ? (
+          <div className="load-more">
+            <button className="btn btn-lg" onClick={loadMore}>Load more...</button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
 }
 
 function mapState({ file }) {
-  return {
-    list: file.list,
-    count: file.count
-  };
+  return file;
 }
 
 export default connect(mapState)(Files);
