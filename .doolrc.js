c9c4f328f1cd78243268ad0789514ec7074ab997
helloyou2012 <helloyou2012@yeah.net>
2018-01-30 14:29:55 +0800

    add public path

--- .doolrc.js
+++ .doolrc.js
@@ -1,9 +1,12 @@
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = {
   entry: {
     index: './src/index.js',
     'download.css': './src/download.less',
     'error.css': './src/error.less'
   },
+  publicPath: prod ? process.env.BUGU_STATIC : '/',
   babelPlugins: ['transform-runtime'],
   devServer: {
     historyApiFallback: {
