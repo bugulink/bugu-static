17167e6647f2e2e039e04d013068b7aa6029b951
helloyou2012 <helloyou2012@yeah.net>
2019-08-28 12:47:47 +0800

    update deps

---
+++ up-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  region: 'qn:cn-east-1',
+  bucket: process.env.BUGU_STATIC_BUCKET,
+  accessKey: process.env.BUGU_STATIC_ACCESS_KEY,
+  secretKey: process.env.BUGU_STATIC_SECRET_KEY
+};
