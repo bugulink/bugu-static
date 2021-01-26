5951e6f5b5f3991ce79118c6bff655ce770b2872
helloyou2012 <helloyou2012@yeah.net>
2021-01-26 16:40:25 +0800

    up config changed

--- up-config.js
+++ up-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  region: 'qn:cn-east-1',
+  region: process.env.BUGU_STATIC_REGION || 'qn:cn-south-1',
   bucket: process.env.BUGU_STATIC_BUCKET,
   accessKey: process.env.BUGU_STATIC_ACCESS_KEY,
   secretKey: process.env.BUGU_STATIC_SECRET_KEY
